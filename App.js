682223afd54b08147831ecdc4612c95bd8fa8fe9
louangeliza <mbabazilouangeliza@gmail.com>
2025-05-27 10:20:20 +0200

    Automated commit on 27/05/2025  9:03:53.59

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <AppNavigator />
-    </PaperProvider>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
